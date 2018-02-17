428be5e3518bb9c8c6775e871ad4e111828f6512
rashemihmih <mihmih96@gmail.com>
2018-02-18 00:27:10 +0300

    kill signal

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,7 +11,7 @@ git push deploy frontend
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o "StrictHostKeyChecking no" apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
-  kill -9 `lsof -t -i:3000`
+  kill `lsof -t -i:3000`
   npm install
   npm run build
   serve -s build -p 3000
