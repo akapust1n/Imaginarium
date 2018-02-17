e936fad80277cc4683778ae609af1eec5ea98a4d
Alexey Kapustin <djkah11@yandex.ru>
2018-02-17 20:41:02 +0300

    try to deploy to remote #10

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
+chmod 600 .travis/travis # Allow read access to the private key
+ssh-add .travis/travis # Add the private key to SSH
 
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-git push deploy master
+git push deploy frontend
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh apps@$IP -p $PORT <<EOF
+ssh -o "StrictHostKeyChecking no" apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
   npm install
   npm start
