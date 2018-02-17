5ab55067627e7ced4f8c10667263b75e9cb506eb
rashemihmih <mihmih96@gmail.com>
2018-02-18 00:22:25 +0300

    production build #11

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,8 +11,8 @@ git push deploy frontend
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o "StrictHostKeyChecking no" apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
-  npm install
-  cd src
   kill -9 `lsof -t -i:3000`
-  npm start
+  npm install
+  npm run build
+  serve -s build -p 3000
 EOF
