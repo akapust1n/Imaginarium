4ab51902da78f4b87d2c2622b4af35583b610486
Alexey Kapustin <djkah11@yandex.ru>
2018-02-18 17:46:19 +0300

    minor deploy git fix #9

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,17 +4,17 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/travis # Allow read access to the private key
 ssh-add .travis/travis # Add the private key to SSH
 
-git config --global push.default matching
-git remote add deploy ssh://apps@$IP:$PORT$WDEPLOY_DIR
-git push deploy backend
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o "StrictHostKeyChecking no" apps@$IP -p $PORT <<EOF
   pwd
   cd $WDEPLOY_DIR
-  pwd
+  git reset --hard origin/backend
+  git pull  origin backend
   cd backend
+  rm *.o Makefile
   qmake
   make
-  ./main
+  pkill -f backend
+  nohup ./backend > log.out 2> log.err < /dev/null &
 EOF
