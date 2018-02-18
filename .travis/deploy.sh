a04ad075c44ba181251ae31c8efda422ed2bcde0
Alexey Kapustin <djkah11@yandex.ru>
2018-02-18 03:13:43 +0300

    fix push to remote rep #10

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,7 +5,7 @@ chmod 600 .travis/travis # Allow read access to the private key
 ssh-add .travis/travis # Add the private key to SSH
 
 git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$WDEPLOY_DIR
+git remote add deploy ssh://apps@$IP:$PORT$WDEPLOY_DIR
 git push deploy backend
 
 # Skip this command if you don't need to execute any additional commands after deploying.
