20cd7a15c9d21d4d3493d0fc3688bdd4217ceb03
Alexey Kapustin <djkah11@yandex.ru>
2018-02-18 03:12:42 +0300

    push to remote rep fix #10 #12

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,15 +4,15 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/travis # Allow read access to the private key
 ssh-add .travis/travis # Add the private key to SSH
 
-git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-git push deploy frontend
-
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o "StrictHostKeyChecking no" apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
-  kill `lsof -t -i:3000`
+  git reset --hard origin/frontend
+  git pull  origin frontend
+  pkill -f serve
   npm install
   npm run build
-  serve -s build -p 3000
+  serve -s build -p 3000 &
+  exit
 EOF
+travis_terminate 0
