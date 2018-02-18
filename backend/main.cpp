d331dfa6535a69e802769e9020ce1db9d2241197
Alexey Kapustin <djkah11@yandex.ru>
2018-02-18 19:56:55 +0300

    Allow cors fix #17 fix #16

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,15 +1,19 @@
 #include "crow.h"
+using namespace crow;
 
 int main()
 {
     crow::SimpleApp app;
     CROW_ROUTE(app, "/")([](){
-        return "Hello world1";
+        response res;
+        res.add_header("Access-Control-Allow-Origin", "*");
+        res.body= "Hello world!";
+        return res;
     });
 
     CROW_ROUTE(app, "/auth")([](){
         return crow::response(200);
     });
 
-    app.port(5000).multithreaded().run();
+    app.port(5001).multithreaded().run();
 }
