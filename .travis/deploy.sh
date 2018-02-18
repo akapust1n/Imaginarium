f7936e4dc29d3179985012b24bffa9604a0b011d
rashemihmih <mihmih96@gmail.com>
2018-02-18 15:18:31 +0300

    fix endless build #12

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,14 +5,12 @@ chmod 600 .travis/travis # Allow read access to the private key
 ssh-add .travis/travis # Add the private key to SSH
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh -o "StrictHostKeyChecking no" apps@$IP -p $PORT <<EOF
+ssh apps@$IP -p $PORT << EOF
   cd $DEPLOY_DIR
   git reset --hard origin/frontend
   git pull  origin frontend
   pkill -f serve
   npm install
   npm run build
-  serve -s build -p 3000 &
-  exit
+  nohup serve -s build -p 3000 > /dev/null 2> /dev/null < /dev/null &
 EOF
-travis_terminate 0
