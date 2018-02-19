bab37c2717e659cf00bb37359863aa1119ee429f
rashemihmih <mihmih96@gmail.com>
2018-02-19 23:34:34 +0300

    args and socket moved to window #19

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios'
 import './App.css';
-import Socket from './socket'
 import Rules from './Rules'
 import Loading from './Loading'
 
 class App extends Component {
     constructor() {
         super();
-        this.vars = {};
-        window.location.search.substring(1).split('&').forEach(pair => {
-            let pairSplit = pair.split('=');
-            this.vars[pairSplit[0]] = pairSplit[1];
-        });
-        this.socket = Socket.getInstance();
         this.joinGame = this.joinGame.bind(this);
     }
 
@@ -36,17 +29,17 @@ class App extends Component {
         ReactDOM.unmountComponentAtNode(document.getElementById('root'));
         let loading = ReactDOM.render(<Loading/>, document.getElementById('root'));
         loading.setStatus('Авторизация...');
-        this.socket.addHandler('Error', content => loading.setStatus(content));
-        this.socket.addHandler('Queue', () => loading.setStatus('Подбор игроков...'));
+        window.socket.addHandler('Error', content => loading.setStatus(content));
+        window.socket.addHandler('Queue', () => loading.setStatus('Подбор игроков...'));
         let data = {
             'type': 'GameJoin',
             'content': {
-                'viewer_id': this.vars['viewer_id'],
-                'api_id': this.vars['api_id'],
-                'auth_key': this.vars['auth_key']
+                'viewer_id': window.vars['viewer_id'],
+                'api_id': window.vars['api_id'],
+                'auth_key': window.vars['auth_key']
             }
         };
-        this.socket.send(JSON.stringify(data));
+        window.socket.send(JSON.stringify(data));
     }
 }
 
