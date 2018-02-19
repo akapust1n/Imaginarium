b47f00ee025617e6571a09561e0f519b9abcc337
rashemihmih <mihmih96@gmail.com>
2018-02-19 03:11:33 +0300

    send GameJoin message #20

--- src/App.js
+++ src/App.js
@@ -15,7 +15,25 @@ class App extends Component {
         this.state = {
             username: ''
         };
-        this.handleClick = this.handleClick.bind(this)
+        this.socket = new WebSocket("wss://kapust1n.ru:5000/ws");
+        this.socket.onopen = function () {
+            console.log("Соединение установлено.");
+        };
+        this.socket.onclose = function (event) {
+            if (event.wasClean) {
+                console.log('Соединение закрыто чисто');
+            } else {
+                console.log('Обрыв соединения');
+            }
+            console.log('Код: ' + event.code + ' причина: ' + event.reason);
+        };
+        this.socket.onmessage = function (event) {
+            console.log("Получены данные " + event.data);
+        };
+        this.socket.onerror = function (error) {
+            console.log("Ошибка " + error.message);
+        };
+        this.joinGame = this.joinGame.bind(this)
     }
 
     render() {
@@ -28,19 +46,22 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
-                <button className='button' onClick={this.handleClick}>Click Me</button>
+                <button className='button' onClick={this.joinGame}>Играть</button>
                 <p>{this.state.username}</p>
             </div>
         );
     }
 
-    handleClick() {
-        console.log(this.vars);
-        console.log(this.vars['viewer_id']);
-        axios.get('https://kapust1n.ru:5000/').then(
-            response => this.setState({username: response.data}),
-            error => console.log(error)
-        );
+    joinGame() {
+        let data = {
+            'type': 'GameJoin',
+            'content': {
+                'viewer_id': this.vars['viewer_id'],
+                'api_id': this.vars['api_id'],
+                'auth_key': this.vars['auth_key']
+            }
+        };
+        this.socket.send(JSON.stringify(data));
     }
 }
 
