6c9955b733cd965a21c87d4448021da1a932d2e3
rashemihmih <mihmih96@gmail.com>
2018-02-19 18:09:38 +0300

    close socket in test #26

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+    let app = ReactDOM.render(<App/>, div);
+    app.socket.close();
     ReactDOM.unmountComponentAtNode(div);
 });
