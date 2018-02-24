963237a680b86c6ed6bf69b6638356c3087fe6f9
rashemihmih <mihmih96@gmail.com>
2018-02-24 18:56:37 +0300

    redundant parenthesis in lambdas

--- src/users.js
+++ src/users.js
@@ -4,15 +4,13 @@ import vars from './vars'
 function getNames(users) {
     let ids = users.map(user => user.viewer_id);
     axios.get(`https://api.vk.com/method/users.get?access_token=${vars['access_token']}&user_ids=${ids}`).then(
-        (response) => {
+        response => {
             for (let i = 0; i < users.length; i++) {
                 users[i].name = response.data.response[i].first_name;
             }
             return users;
         },
-        (error) => {
-            console.log(error);
-        }
+        error => console.log(error)
     );
 }
 
