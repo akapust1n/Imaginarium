06c692bfc0d6e4788180482531187719da0d715d
rashemihmih <mihmih96@gmail.com>
2018-02-24 20:34:06 +0300

    call vk api with jsonp #35

--- src/users.js
+++ src/users.js
@@ -1,17 +1,30 @@
-import axios from 'axios'
 import vars from './vars'
+import fetchJsonp from 'fetch-jsonp'
 
 function getNames(users) {
-    let ids = users.map(user => user.viewer_id);
-    axios.get(`https://api.vk.com/method/users.get?access_token=${vars['access_token']}&user_ids=${ids}`).then(
-        response => {
-            for (let i = 0; i < users.length; i++) {
-                users[i].name = response.data.response[i].first_name;
+    // let ids = users.map(user => user.viewer_id);
+    // axios.get(`https://api.vk.com/method/users.get?access_token=${vars['access_token']}&user_ids=${ids}`).then(
+    //     response => {
+    //         for (let i = 0; i < users.length; i++) {
+    //             users[i].name = response.data.response[i].first_name;
+    //         }
+    //         return users;
+    //     },
+    //     error => console.log(error)
+    // );
+    fetchJsonp(`https://api.vk.com/method/users.get?access_token=${vars['access_token']}&user_ids=${ids}`)
+        .then(response => response.json())
+        .then(json => {
+            console.log('parsed json', json);
+            {
+                for (let i = 0; i < users.length; i++) {
+                    let user = json.response[i];
+                    users[i].name = user.first_name + ' ' + user.last_name;
+                }
+                return users;
             }
-            return users;
-        },
-        error => console.log(error)
-    );
+        })
+        .catch(ex => console.log(ex))
 }
 
 export default getNames;
