26a73e97d0d6fb07e1ceeb3f71306b193e2420df
rashemihmih <mihmih96@gmail.com>
2018-02-27 02:11:45 +0300

    turn end #38

--- src/TurnEnd.js
+++ src/TurnEnd.js
@@ -2,25 +2,24 @@ import React, {Component} from 'react';
 import getNames from "./users";
 import './Game.css'
 import './TurnEnd.css'
-import vars from "./vars";
+import socket from "./socket";
 
 class TurnEnd extends Component {
     constructor(props) {
         super(props);
         this.state = {};
+        TurnEnd.nextTurn = TurnEnd.nextTurn.bind(this);
     }
 
     componentWillMount() {
-        // let data = this.props.data;
-        // let cards = data.cards;
-        // let master = data.master;
-        // this.setState(data);
-        // getNames(cards.players, () => this.setState({cards: cards}));
-        // getNames([master], result => this.setState({master: result[0]}));
-        // this.setState({
-        //     association: this.props.association,
-        //     selected: cards.find(card => card.isCorrect)
-        // });
+        let data = this.props.data;
+        let cards = data.cards;
+        let master = data.master;
+        data.association = this.props.association;
+        data.selected = cards.find(card => card.isCorrect);
+        this.setState(data);
+        cards.forEach(card => getNames(card.players, () => this.setState({cards: cards})));
+        getNames([master], result => this.setState({master: result[0]}));
     }
 
     render() {
@@ -28,32 +27,56 @@ class TurnEnd extends Component {
             <div className='container'>
                 <div className='row'>
                     <div className='col-4 Game-card-big center-content'>
-                        <img height={'100%'} src='https://kapust1n.ru:5000/files/cards/049.jpg'/>
+                        <img height={'100%'} src={this.state.selected.card_url}/>
                     </div>
                     <div className='col-8'>
-                        <div className='row'>
-                            <div className='col-1'>
-                                <button className='TurnEnd-btn-card'>
-                                    <img className='Game-card' src='https://kapust1n.ru:5000/files/cards/049.jpg'/>
-                                </button>
-                            </div>
-                            <div className='col-11'>
-                                <div className='TurnEnd-player-name'>Колян Николаев (+4),</div>
-                                <div className='TurnEnd-player-name'>Колян Николаев (+4),</div>
-                                <div className='TurnEnd-player-name'>Колян Николаев (+4),</div>
-                                <div className='TurnEnd-player-name'>Колян Николаев (+4),</div>
-                                <div className='TurnEnd-player-name'>Колян Николаев (+4),</div>
-                            </div>
-                        </div>
+                        {this.state.cards.map(card => <Card data={card} parent={this}/>)}
                     </div>
                 </div>
                 <div className='row'>
                     <div className='col-10'>
-                        Вася Васильев (+4):<br/>Какой ценой режиссер Ридли Скотт решил все проблемы, которые возникли вокруг его нового фильма «Все деньги мира».
+                        {this.state.master.name} (+{this.state.master.gain}):<br/>{this.state.association}
                     </div>
                     <div className='col-2 center-content'>
-                        <button className='btn btn-primary'>Продолжить</button>
+                        <button onClick={TurnEnd.nextTurn} className='btn btn-primary'>Продолжить</button>
                     </div>
+                </div>
+            </div>
+        );
+    }
+
+    selectCard(card) {
+        this.setState({selected: card})
+    }
+
+    static nextTurn() {
+        let data = {'type': 'NextTurn'};
+        console.log('sending', JSON.stringify(data));
+        socket.send(JSON.stringify(data));
+    }
+}
+
+class Card extends Component {
+    render() {
+        let card = this.props.data;
+        let parent = this.props.parent;
+        let className = 'row';
+        if (card.isCorrect) {
+            className += ' TurnEnd-correct'
+        }
+        return (
+            <div className={className}>
+                <div className='col-1'>
+                    <button onClick={parent.selectCard.bind(parent, card)} className='TurnEnd-btn-card'>
+                        <img className='Game-card' src={card.card_url}/>
+                    </button>
+                </div>
+                <div className='col-11'>
+                    {card.players.map((player, i, players) =>
+                        <div className='TurnEnd-player-name'>
+                            {player.name} (+{player.gain}){i === players.length - 1 ? '' : ','}
+                        </div>
+                    )}
                 </div>
             </div>
         );
