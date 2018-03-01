3995b682dbb0fad7d5c9b057ddfbfc1efa30bd76
Alexey Kapustin <djkah11@yandex.ru>
2018-03-01 16:58:57 +0300

    fix #46

--- backend/Match.cpp
+++ backend/Match.cpp
@@ -9,14 +9,13 @@ Match::Match(int _maxSize, CardHolder& cardHolder)
     : maxSize(_maxSize)
     , dropedCards(0)
     , guessCards(0)
-    , nextTurnCounter(0),
-      masterNum(0)
+    , nextTurnCounter(0)
+    , masterNum(0)
 {
     mutex.reset(new std::mutex());
-    deck = cardHolder.getDeck(60);
+    deck = cardHolder.getDeck(20);
     phase = Phase::BeforeStart;
 }
-
 
 bool Match::isFull() const
 {
@@ -82,10 +81,10 @@ bool Match::dropCard(int cardId, PlayerSP player)
 {
     if (player->dropCard(cardId)) {
         mutex->lock();
-        std::cout<<"\nDROP_CARD \n";
+        std::cout << "\nDROP_CARD \n";
         dropedCards++;
 
-        if (dropedCards == maxSize -1 ) {
+        if (dropedCards == maxSize - 1) {
             dropedCards = 0;
             mutex->unlock();
             return true;
@@ -102,7 +101,7 @@ bool Match::guessCard(int cardId, PlayerSP player)
     mutex->lock();
     guessCards++;
 
-    if (guessCards == maxSize -1 ) {
+    if (guessCards == maxSize - 1) {
         guessCards = 0;
         mutex->unlock();
         return true;
@@ -119,7 +118,7 @@ bool Match::nextTurn(PlayerSP player)
     mutex->lock();
     nextTurnCounter++;
 
-    if (nextTurnCounter == maxSize  ) {
+    if (nextTurnCounter == maxSize) {
         nextTurnCounter = 0;
         mutex->unlock();
         return true;
@@ -136,8 +135,14 @@ int Match::getDropedCards() const
 
 bool Match::prepareTurn()
 {
-    if (deck.empty()){
-        return false;
+    if (deck.empty()) {
+        int counterEmpty = 0;
+        for (auto& player : players) {
+            if (player->getHand().empty())
+                counterEmpty++;
+            if (counterEmpty == players.size())
+                return false;
+        }
     }
     dropedCards = 0;
     guessCards = 0;
@@ -145,14 +150,13 @@ bool Match::prepareTurn()
     nextTurnCounter = 0;
     players[masterNum]->setIsMaster(false);
     masterNum = (masterNum + 1) % maxSize;
-    master=players[masterNum]->getViewer_id();
+    master = players[masterNum]->getViewer_id();
     players[masterNum]->setIsMaster(true);
     for (int i = 0; i < maxSize; i++) {
         players[i]->addCard(deck.back());
         deck.pop_back();
     }
     return true;
-
 }
 
 Match::Phase Match::getPhase() const
@@ -160,7 +164,7 @@ Match::Phase Match::getPhase() const
     return phase;
 }
 
-void Match::setPhase(const Phase &value)
+void Match::setPhase(const Phase& value)
 {
     phase = value;
 }
